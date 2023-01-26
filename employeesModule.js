45f6a3c59630c30247bb29ec6592166820949152
ZiaulHaq <z.fazli@hrmis.gov.af>
2023-01-26 10:58:34 +0430

    Module Export & Require

---
+++ employeesModule.js
@@ -0,0 +1,9 @@
+//Array of employees
+const employees = ['Ahmad','Jan','Sulaiman','Momand','Baryali'];
+const designations = ['Sofware Developer','Designer','Software Engineer','Quality Assurance','Team Lead']
+
+//Syntax to make a module exportable by outside 
+module.exports = { 
+    employees,
+    designations
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import the employees module 
+const { employees, designations } = require('./employeesModule')
+
+
+//Console the employees array from the employees module.
+console.log(employees, designations)
